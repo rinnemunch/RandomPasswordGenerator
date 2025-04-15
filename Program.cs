045814d0678fa02f28735f1aca827bb2d71ed38c
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-15 12:04:19 -0400

    Generator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+Random rand = new Random(); 
+int passwordLength = 10;
+string password = ""; 
+
+for (int i = 0; i < passwordLength; i++)
+{
+    int index = rand.Next(characters.Length);
+    password += characters[index];
+}
